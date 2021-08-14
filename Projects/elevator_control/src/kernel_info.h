532b321d2929a74a66e94740cd1cc5b0f4b93bae
Danilo Fuchs <danilo_fuchs@hotmail.com>
2021-08-14 19:57:14 -0300

    fix: endif should be at end of header file

--- Projects/elevator_control/src/kernel_info.h
+++ Projects/elevator_control/src/kernel_info.h
@@ -1,6 +1,5 @@
 #ifndef __SYSTEM_INFO_H_
 #define __SYSTEM_INFO_H_
-#endif
 
 // Defines the maximum amount of threads to log
 #define SYSTEM_INFO_THREAD_COUNT_MAX 8
@@ -8,3 +7,5 @@
 void printKernelInfo(void);
 void printKernelState(void);
 void printThreadInfo(void);
+
+#endif
--- Projects/elevator_control/src/uart.h
+++ Projects/elevator_control/src/uart.h
@@ -1,9 +1,10 @@
 #ifndef __UART_H_
 #define __UART_H_
-#endif
 
 #define UART_TX_BUFFER_SIZE 1024
 #define UART_BAUD_RATE 115200
 
 void UARTInit(void);
 void UART0_Handler(void);
+
+#endif
