ece22e2add6f5422faf91fe1f29f0170469041ab
Danilo Fuchs <danilo_fuchs@hotmail.com>
2021-08-20 22:31:45 -0300

    feat: Basic state machine to go Up

--- Projects/elevator_control/src/elevator.h
+++ Projects/elevator_control/src/elevator.h
@@ -14,9 +14,27 @@ typedef enum {
   elevator_direction_down,
 } elevator_direction_t;
 
+typedef enum {
+  elevator_state_uninitialized,
+  elevator_state_idle,
+  elevator_state_moving,
+  elevator_state_closing_doors,
+  elevator_state_opening_doors,
+  elevator_state_awaiting_passengers,
+} elevator_state_t;
+
+typedef enum {
+  elevator_door_state_closed,
+  elevator_door_state_open,
+} elevator_door_state_t;
+
 typedef struct {
   elevator_code_t code;
+  elevator_state_t state;
+
+  int8_t floor;
   elevator_direction_t direction;
+  elevator_door_state_t door_state;
 } elevator_t;
 
 #endif
--- Projects/elevator_control/src/elevator_thread.c
+++ Projects/elevator_control/src/elevator_thread.c
@@ -14,6 +14,8 @@ static void sendCommand(command_t* command, osMutexId_t mutex) {
 }
 
 static void initialize(elevator_t* elevator, osMutexId_t mutex) {
+  elevator->state = elevator_state_idle;
+
   command_t command = {
       .code = command_initialize,
       .elevator_code = elevator->code,
@@ -45,11 +47,51 @@ static void turnButtonOff(elevator_t* elevator, int8_t floor,
   sendCommand(&command, mutex);
 }
 
+static void reachedFloor(elevator_t* elevator, int8_t floor,
+                         osMutexId_t mutex) {
+  if (floor < 0) return;
+  elevator->floor = floor;
+}
+
+static void closeDoors(elevator_t* elevator, osMutexId_t mutex) {
+  elevator->state = elevator_state_closing_doors;
+
+  command_t command = {
+      .code = command_close_doors,
+      .elevator_code = elevator->code,
+      .floor = -1,
+  };
+
+  sendCommand(&command, mutex);
+}
+
+static void goUp(elevator_t* elevator, osMutexId_t mutex) {
+  elevator->state = elevator_state_moving;
+  elevator->direction = elevator_direction_up;
+
+  command_t command = {
+      .code = command_go_up,
+      .elevator_code = elevator->code,
+      .floor = -1,
+  };
+
+  sendCommand(&command, mutex);
+}
+
+static void doorsAreClosed(elevator_t* elevator, osMutexId_t mutex) {
+  goUp(elevator, mutex);
+}
+
 void elevatorThread(void* arg) {
   elevator_thread_t* this = (elevator_thread_t*)arg;
+  osMutexId_t mutex = this->args.uart_write_mutex;
+
   elevator_t elevator = {
       .code = this->args.code,
+      .state = elevator_state_uninitialized,
+      .floor = 0,
       .direction = elevator_direction_unknown,
+      .door_state = elevator_door_state_closed,
   };
 
   signal_t signal;
@@ -61,10 +103,17 @@ void elevatorThread(void* arg) {
 
     switch (signal.code) {
       case signal_system_initialized:
-        initialize(&elevator, this->args.uart_write_mutex);
+        initialize(&elevator, mutex);
         break;
       case signal_internal_button_pressed:
-        turnButtonOn(&elevator, signal.floor, this->args.uart_write_mutex);
+        turnButtonOn(&elevator, signal.floor, mutex);
+        closeDoors(&elevator, mutex);
+        break;
+      case signal_doors_closed:
+        doorsAreClosed(&elevator, mutex);
+        break;
+      case signal_reached_floor:
+        reachedFloor(&elevator, signal.floor, mutex);
         break;
     }
   }
